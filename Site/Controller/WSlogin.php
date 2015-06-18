3cea0d2fa8afe580e91f217058d9e4ec611b8171
Florian Olivier-Koehret <florian@paperus.fr>
2015-06-18 08:28:38 +0200

    login rendu fonctionnel

--- Site/Controller/WSlogin.php
+++ Site/Controller/WSlogin.php
@@ -1,16 +1,25 @@
 <?php
 // TODO : rendre fonctionnel
-$bool = true;
-
-header('Access-Control-Allow-Origin: *');
-if ($bool){
-	header('Content-type: application/json');
-    //renvoie une valeur permettant de déterminer si l'utilisateur a les droits de scan
-	echo("1");
-}else{
-	header("HTTP/1.1 401 Unauthorized");
-	exit;
-}	
+	include '../Model/DataModel/user_full_DM.php';
+	include('../Model/ViewModel/user_list_login.php');
+    $userslogin = new user_list_login();
+    foreach($userslogin->user_list as $value)
+    {
+        if($_POST['user_uuid']==$value->user_uuid && $_POST['user_password']==$value->user_password)
+        {
+            $_SESSION['user_user_type'] = $value->user_user_type;
+            $_SESSION['user_surname'] = $value->user_surname;
+            $_SESSION['user_uuid'] = $value->user_uuid;
+            $_SESSION['user_is_admin'] = $value->user_is_admin;
+            $_SESSION['user_pk'] = $value->user_pk;
+            header('Content-type: application/json');
+            echo($value->user_user_type);
+            //ob_end_clean();
+            break;
+        }
+        else
+        {
+            header("HTTP/1.1 401 Unauthorized");
+        }
+    }
 ?>
-
-	
